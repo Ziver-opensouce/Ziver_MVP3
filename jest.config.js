bb960d27e6727a264aa088ddc1a2c870f3cf109c
Zaidu Abubakar <zaiduabubakar777@gmail.com>
2025-07-09 16:07:32 +0100

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,1 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
-  // Use the ts-jest preset to automatically handle TypeScript transformations.
-  preset: 'ts-jest',
 
-  // The testing environment. 'node' is correct for your use case.
-  testEnvironment: 'node',
-
-  // Keep your specific testMatch pattern to tell Jest where to find your tests.
-  testMatch: [
-    '<rootDir>/contracts/tests/**/*.spec.ts'
-  ],
-};
