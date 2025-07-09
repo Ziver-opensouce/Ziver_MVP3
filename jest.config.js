a953d2aedd9dc0aa346a51f4e98bd20868e5615a
Zaidu Abubakar <zaiduabubakar777@gmail.com>
2025-07-09 08:44:58 +0100

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,11 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: 'ts-jest',
   testEnvironment: 'node',
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', {
+      tsconfig: 'tsconfig.json'
+    }]
+  },
   testMatch: [
     '<rootDir>/contracts/tests/**/*.spec.ts'
   ],
