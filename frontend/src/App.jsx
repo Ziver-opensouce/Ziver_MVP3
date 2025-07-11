98baa9fac88c6650530197c2b15444b7afd4b3cd
Zaidu Abubakar <zaiduabubakar777@gmail.com>
2025-07-11 07:31:08 +0100

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,44 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+
+// Import all the pages
+import LandingPage from './pages/LandingPage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import MiningPage from './pages/MiningPage';
 
+// Import the layout and protection components
+import ProtectedRoute from './components/layout/ProtectedRoute';
+import MainAppLayout from './components/layout/MainAppLayout'; // We will create this next
+
 function App() {
   return (
     <Routes>
-      {/* Set the mining page as the default homepage for now */}
-      <Route path="/" element={<MiningPage />} /> 
+      {/* === Public Routes === */}
+      <Route path="/" element={<LandingPage />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
+
+      {/* === Protected Application Routes === */}
+      {/* All app routes will be nested under /app and will have the main layout */}
+      <Route 
+        path="/app" 
+        element={
+          <ProtectedRoute>
+            <MainAppLayout />
+          </ProtectedRoute>
+        }
+      >
+        {/* The "index" route is the default page for /app */}
+        <Route index element={<MiningPage />} /> 
+        <Route path="mining" element={<MiningPage />} />
+        
+        {/* We will create these page components soon */}
+        {/* <Route path="tasks" element={<TasksPage />} /> */}
+        {/* <Route path="jobs" element={<MicroJobsPage />} /> */}
+        {/* <Route path="profile" element={<ProfilePage />} /> */}
+      </Route>
       
-      {/* We will add more routes for Tasks, Jobs, etc. here later */}
     </Routes>
   );
 }
