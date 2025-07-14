da3981eb7e1b74647e336fbed2eea87cc091de7c
Zaidu Abubakar <zaiduabubakar777@gmail.com>
2025-07-14 01:42:37 +0100

    Update Minimal.spec.ts

--- contracts/tests/Minimal.spec.ts
+++ contracts/tests/Minimal.spec.ts
@@ -2,7 +2,7 @@ import { Blockchain, SandboxContract, TreasuryContract } from '@ton/sandbox';
 import { Address, beginCell, Cell, Contract, contractAddress, Sender, toNano } from '@ton/core';
 import '@ton/test-utils';
 import { compile } from '@ton/blueprint';
-import { MinimalContract } from '../wrappers/Minimal'; // We will create this wrapper
+import { MinimalContract } from '../wrappers/MinimalContract'; // FIX: The filename is MinimalContract
 
 describe('Minimal Contract Test', () => {
     let blockchain: Blockchain;
@@ -12,14 +12,13 @@ describe('Minimal Contract Test', () => {
 
     beforeAll(async () => {
         // Compile the new minimal contract
-        code = await compile('MinimalContract'); 
+        code = await compile('MinimalContract');
     });
 
     beforeEach(async () => {
         blockchain = await Blockchain.create();
         deployer = await blockchain.treasury('deployer');
 
-        // This is a more standard way to deploy
         minimalContract = blockchain.openContract(MinimalContract.createFromConfig({}, code));
 
         const deployResult = await minimalContract.sendDeploy(deployer.getSender(), toNano('0.05'));
@@ -37,7 +36,6 @@ describe('Minimal Contract Test', () => {
     });
 
     it('should set and get a new value', async () => {
-        // Send a message to set the value to 123
         const setValueResult = await minimalContract.sendValue(deployer.getSender(), 123n);
 
         expect(setValueResult.transactions).toHaveTransaction({
@@ -46,7 +44,6 @@ describe('Minimal Contract Test', () => {
             success: true,
         });
 
-        // Check if the value was updated correctly
         expect(await minimalContract.getValue()).toEqual(123n);
     });
 });
