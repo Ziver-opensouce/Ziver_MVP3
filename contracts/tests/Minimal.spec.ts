2e1ee5e01c9d78892886aca6aa9a051e4e94f863
Zaidu Abubakar <zaiduabubakar777@gmail.com>
2025-07-14 01:24:27 +0100

    Update Minimal.spec.ts

--- contracts/tests/Minimal.spec.ts
+++ contracts/tests/Minimal.spec.ts
@@ -1,41 +1,52 @@
-import { Address, beginCell, Cell, Contract, contractAddress, ContractProvider, Sender, SendMode } from '@ton/core';
+import { Blockchain, SandboxContract, TreasuryContract } from '@ton/sandbox';
+import { Address, beginCell, Cell, Contract, contractAddress, Sender, toNano } from '@ton/core';
+import '@ton/test-utils';
+import { compile } from '@ton/blueprint';
+import { MinimalContract } from '../wrappers/Minimal'; // We will create this wrapper
 
-export type MinimalContractConfig = {};
+describe('Minimal Contract Test', () => {
+    let blockchain: Blockchain;
+    let deployer: SandboxContract<TreasuryContract>;
+    let minimalContract: SandboxContract<MinimalContract>;
+    let code: Cell;
 
-export function minimalContractConfigToCell(config: MinimalContractConfig): Cell {
-    return beginCell().endCell();
-}
+    beforeAll(async () => {
+        // Compile the new minimal contract
+        code = await compile('MinimalContract'); 
+    });
 
-export class MinimalContract implements Contract {
-    constructor(readonly address: Address, readonly init?: { code: Cell; data: Cell }) {}
+    beforeEach(async () => {
+        blockchain = await Blockchain.create();
+        deployer = await blockchain.treasury('deployer');
 
-    static createFromConfig(config: MinimalContractConfig, code: Cell, workchain = 0) {
-        const data = minimalContractConfigToCell(config);
-        const init = { code, data };
-        return new MinimalContract(contractAddress(workchain, init), init);
-    }
+        // This is a more standard way to deploy
+        minimalContract = blockchain.openContract(MinimalContract.createFromConfig({}, code));
 
-    async sendDeploy(provider: ContractProvider, via: Sender, value: bigint) {
-        await provider.internal(via, {
-            value,
-            sendMode: SendMode.PAY_GAS_SEPARATELY,
-            body: beginCell().endCell(),
+        const deployResult = await minimalContract.sendDeploy(deployer.getSender(), toNano('0.05'));
+
+        expect(deployResult.transactions).toHaveTransaction({
+            from: deployer.address,
+            to: minimalContract.address,
+            deploy: true,
+            success: true,
         });
-    }
+    });
 
-    async sendValue(provider: ContractProvider, via: Sender, valueToSet: bigint) {
-        await provider.internal(via, {
-            value: toNano('0.05'),
-            sendMode: SendMode.PAY_GAS_SEPARATELY,
-            body: beginCell()
-                .storeUint(0x1, 32) // op
-                .storeUint(valueToSet, 64)
-                .endCell(),
+    it('should deploy and have initial value of 0', async () => {
+        expect(await minimalContract.getValue()).toEqual(0n);
+    });
+
+    it('should set and get a new value', async () => {
+        // Send a message to set the value to 123
+        const setValueResult = await minimalContract.sendValue(deployer.getSender(), 123n);
+
+        expect(setValueResult.transactions).toHaveTransaction({
+            from: deployer.address,
+            to: minimalContract.address,
+            success: true,
         });
-    }
 
-    async getValue(provider: ContractProvider) {
-        const result = await provider.get('get_value', []);
-        return result.stack.readBigNumber();
-    }
-}
+        // Check if the value was updated correctly
+        expect(await minimalContract.getValue()).toEqual(123n);
+    });
+});
